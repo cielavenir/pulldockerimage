0aac986b6bce9e360a928cb0bf07c6cdeeb29038
cielavenir <cielartisan@gmail.com>
2019-05-21 01:05:22 +0900

    rewritten to support index.docker.io properly

--- pulldockerimage.py
+++ pulldockerimage.py
@@ -10,14 +10,17 @@ import sys
 import base64
 import hashlib
 import tarfile
-from contextlib import closing
+from contextlib import closing, contextmanager
 
 if sys.version_info[0]>=3:
     import http.client as httplib
+    from urllib.request import parse_http_list, parse_keqv_list, urlparse
     from io import BytesIO
     binstdout = sys.stdout.buffer
 else:
     import httplib
+    from urllib2 import parse_http_list, parse_keqv_list
+    from urlparse import urlparse
     from cStringIO import StringIO
     BytesIO = StringIO
     binstdout = sys.stdout
@@ -42,86 +45,134 @@ def makeTarInfo(**kwargs):
         setattr(info,k,kwargs[k])
     return info
 
-if len(sys.argv)<2:
-    sys.stderr.write('pulldockerimage.py host/repository:tag > archive.tar\n')
-    sys.stderr.write('generate a docker image directly (without deploying to the host machine)\n')
-    exit()
-
-repository = sys.argv[1].split(':')[0]
-host = repository.split('/')[0]
-repository = repository[len(host)+1:]
-tag = sys.argv[1].split(':')[1]
-
-with closing(httplib.HTTPSConnection(host)) as https:
-    auth = {}
-    basic = loggedin(host)
-    if basic:
-        account = base64.b64decode(basic).decode('utf-8').split(':')[0]
-        https.request('GET','/auth?account=%s&scope=repository:%s:pull&service=%s'%(account,repository,host),None,{'Authorization':'Basic '+basic})
-        resp = https.getresponse()
-        token = json.load(resp)['token']
-        auth = {'Authorization':'Bearer '+token}
-    https.request('GET','/v2/%s/manifests/%s'%(repository,tag),None,dict(auth,Accept='application/vnd.docker.distribution.manifest.v2+json'))
+@contextmanager
+def ensureResponse(https,auth):
     resp = https.getresponse()
-    manifestv2 = json.load(resp)
-    
-    with tarfile.open(mode='w|',fileobj=binstdout) as tar:
-        https.request('GET','/v2/%s/blobs/%s'%(repository,manifestv2['config']['digest']),None,auth)
-        resp = https.getresponse()
-        tar.addfile(makeTarInfo(
-            name=manifestv2['config']['digest'].split(':')[1]+'.json',
-            size=int(resp.getheader('content-length'))
-        ),resp)
-        manifestjson = {
-            'Config' : manifestv2['config']['digest'].split(':')[1]+'.json',
-            'Repotags' : [
-                '%s/%s:%s'%(host,repository,tag)
-            ],
-            'Layers' : []
-        }
-        layerId = ''
-        for layer in manifestv2['layers']:
-            layerDigest = layer['digest']
-            parentId = layerId
-            layerId = hashlib.sha256((parentId+'\n'+layerDigest+'\n').encode('utf-8')).hexdigest()
-            manifestjson['Layers'].append(layerId+'/layer.tar')
-            sys.stderr.write('%s (%s)\n'%(layerId,layerDigest))
-            tar.addfile(makeTarInfo(name=layerId+'/VERSION',size=3),BytesIO('1.0'.encode('utf-8')))
-            jsonstr = json.dumps({
-                'id' : layerId,
-                'parentId' : parentId,
-                'created' : '0001-01-01T00:00:00Z',
-                'container_config' : {
-                    'Hostname' : '',
-                    'Domainname' : '',
-                    'User' : '',
-                    'AttachStdin' : False,
-                    'AttachStdout' : False,
-                    'AttachStderr' : False,
-                    'Tty' : False,
-                    'OpenStdin' : False,
-                    'StdinOnce' : False,
-                    'Env' : None,
-                    'Cmd' : None,
-                    'Image' : '',
-                    'Volumes' : None,
-                    'WorkingDir' : '',
-                    'Entrypoint' : None,
-                    'OnBuild' : None,
-                    'Labels' : None,
-                }
-            })
-            tar.addfile(makeTarInfo(name=layerId+'/json',data=jsonstr),BytesIO(jsonstr.encode('utf-8')))
-            https.request('GET','/v2/%s/blobs/%s'%(repository,layerDigest),None,auth)
+    if resp.status not in [301,302,307,308]:
+        yield resp
+        return
+    resp.read()
+    while True:
+        location = resp.getheader('location')
+        locationurl = urlparse(location)
+        with closing(httplib.HTTPSConnection(locationurl.netloc)) as https:
+            https.request('GET',locationurl.path+'?'+locationurl.query,None,auth)
             resp = https.getresponse()
-            tar.addfile(makeTarInfo(
-                name=layerId+'/layer.tar',
-                size=int(resp.getheader('content-length'))
-            ),resp)
-        manifestjsonstr = json.dumps([manifestjson])
-        tar.addfile(makeTarInfo(name='manifest.json',data=manifestjsonstr),BytesIO(manifestjsonstr.encode('utf-8')))
-        repositoryjsonstr = json.dumps({
-            host+'/'+repository : {tag : ''}
-        })
-        tar.addfile(makeTarInfo(name='repositories',data=repositoryjsonstr),BytesIO(repositoryjsonstr.encode('utf-8')))
+            if resp.status not in [301,302,307,308]:
+                yield resp
+                return
+            resp.read()
 
+def pullDockerImage(arg,fout):
+    repository = arg.split(':')[0]
+    host = repository.split('/')[0]
+    repository = repository[len(host)+1:]
+    tag = arg.split(':')[1]
+
+    with closing(httplib.HTTPSConnection(host)) as https:
+        auth = {}
+        https.request('GET','/v2/%s/manifests/%s'%(repository,tag),None,dict(auth,Accept='application/vnd.docker.distribution.manifest.v2+json'))
+        resp = https.getresponse()
+        if resp.status == 401:
+            resp.read()
+            authresp = resp.getheader('www-authenticate')
+            realm = parse_keqv_list(parse_http_list(authresp[authresp.index(' ')+1:]))
+            realmurl = urlparse(realm['realm'])
+            basic = loggedin(realmurl.netloc)
+            if basic:
+                account = base64.b64decode(basic).decode('utf-8').split(':')[0]
+                with closing(httplib.HTTPSConnection(realmurl.netloc)) as authhttps:
+                    authhttps.request('GET','%s?account=%s&scope=repository:%s:pull&service=%s'%(realmurl.path,account,repository,realm['service']),None,{'Authorization':'Basic '+basic})
+                    resp = authhttps.getresponse()
+                    token = json.load(resp)['token']
+                    auth = {'Authorization':'Bearer '+token}
+            else:
+                raise Exception('`docker login %s` is required.'%host)
+
+            https.request('GET','/v2/%s/manifests/%s'%(repository,tag),None,dict(auth,Accept='application/vnd.docker.distribution.manifest.v2+json'))
+            resp = https.getresponse()
+
+        manifestv2 = json.load(resp)
+        with tarfile.open(mode='w|',fileobj=fout) as tar:
+            https.request('GET','/v2/%s/blobs/%s'%(repository,manifestv2['config']['digest']),None,auth)
+            with ensureResponse(https,auth) as resp:
+                tar.addfile(makeTarInfo(
+                    name=manifestv2['config']['digest'].split(':')[1]+'.json',
+                    size=int(resp.getheader('content-length'))
+                ),resp)
+            manifestjson = {
+                'Config' : manifestv2['config']['digest'].split(':')[1]+'.json',
+                'RepoTags' : [
+                    '%s/%s:%s'%(host,repository,tag)
+                ],
+                'Layers' : []
+            }
+            layerId = ''
+            for layer in manifestv2['layers']:
+                layerDigest = layer['digest']
+                parentId = layerId
+                layerId = hashlib.sha256((parentId+'\n'+layerDigest+'\n').encode('utf-8')).hexdigest()
+                manifestjson['Layers'].append(layerId+'/layer.tar')
+                sys.stderr.write('%s (%s)\n'%(layerId,layerDigest))
+                tar.addfile(makeTarInfo(name=layerId+'/VERSION',size=3),BytesIO('1.0'.encode('utf-8')))
+                jsonstr = json.dumps({
+                    'id' : layerId,
+                    'parentId' : parentId,
+                    'created' : '0001-01-01T00:00:00Z',
+                    'container_config' : {
+                        'Hostname' : '',
+                        'Domainname' : '',
+                        'User' : '',
+                        'AttachStdin' : False,
+                        'AttachStdout' : False,
+                        'AttachStderr' : False,
+                        'Tty' : False,
+                        'OpenStdin' : False,
+                        'StdinOnce' : False,
+                        'Env' : None,
+                        'Cmd' : None,
+                        'Image' : '',
+                        'Volumes' : None,
+                        'WorkingDir' : '',
+                        'Entrypoint' : None,
+                        'OnBuild' : None,
+                        'Labels' : None,
+                    }
+                })
+                tar.addfile(makeTarInfo(name=layerId+'/json',data=jsonstr),BytesIO(jsonstr.encode('utf-8')))
+                https.request('GET','/v2/%s/blobs/%s'%(repository,layerDigest),None,auth)
+                with ensureResponse(https,auth) as resp:
+                    tar.addfile(makeTarInfo(
+                        name=layerId+'/layer.tar',
+                        size=int(resp.getheader('content-length'))
+                    ),resp)
+
+            manifestjsonstr = json.dumps([manifestjson])
+            tar.addfile(makeTarInfo(name='manifest.json',data=manifestjsonstr),BytesIO(manifestjsonstr.encode('utf-8')))
+            repositoryjsonstr = json.dumps({
+                host+'/'+repository : {tag : ''}
+            })
+            tar.addfile(makeTarInfo(name='repositories',data=repositoryjsonstr),BytesIO(repositoryjsonstr.encode('utf-8')))
+
+    return 0
+
+if __name__ == '__main__':
+    if len(sys.argv)<2:
+        sys.stderr.write(
+'''pulldockerimage.py host/repository:tag > archive.tar
+eg: index.docker.io/library/ubuntu:devel > ubuntu.tar
+
+generate a docker image directly (without deploying to the client machine).
+
+`docker login` is required prior. .docker/config.json should look like this.
+
+{
+        "auths": {
+                "index.docker.io": {
+                        "auth": base64(username:password)
+                }
+        }
+}
+''')
+        exit(1)
+    exit(pullDockerImage(sys.argv[1],binstdout))
--- pulldockerimage.rb
+++ pulldockerimage.rb
@@ -11,7 +11,14 @@ require 'json'
 require 'fileutils'
 require 'digest/sha2'
 require 'base64'
+require 'uri'
+require 'zlib'
 require 'archive/tar/minitar' # gem install minitar
+
+### ruby-mechanize (C) sparklemotion under MIT license.
+### unless https://github.com/sparklemotion/mechanize/issues/495 is nicely resolved, we cannot install the gem safely.
+### This defines Mechanize::HTTP::WWWAuthenticateParser class.
+eval Zlib.inflate Base64.decode64 'eNqlVttu00AQffdXDIbKTRucchGVQkzDTeIBEAIkHuIQbZ1pbNVZp7trAlTl29nZ8S0OFRI8WNmd+5w5s4rCqzJTGGijdCJk4CW50BreYZIKmf3Ep3x/8/nzh0r1vDTpRxT52hPGqIVCsUQFY52kuMZdWamyXYFyfku8gExmJhO5zQDsOQRrPQS2mLIMIqjCTq0SAKyAYk6dFd34kF1UfiiXLnoUQWFSVJ7G/CLkuqNaCEIumwy1MOQ76zhZq+M76zhjq2sT226EBrzKz6zWlZEKncJdGMtiWSRjbzatW526XjnWPCSzpvRM6g0mpuPm3500mMOBhoMNxP6Bjv1nPhzALUEpnvvI9WUq8hzligD9ZFSZmFDiFsa153gjlFhre1Bi24659XOVzebg7Dy2nrmfeVM4A0PjSYTGenDbFCUEL4TOkgCMvXhKZFb9XK3KNUrzWqlCDcHfqOJbtkTyh4tCgfMAUZKLyRJhskL6UMoctXZJGoaOx0TO8bjBiHrrkgqOIRgFQyAuzAJXZjCvCnuVrVAbruy/Q2KusWqvE6rusJSXsthKt0q9xqrQcPeaDzc+obqL7EKKNVbQNymJ+W+IZERu1lnivP/89h1xDwRoozK5coiyusNUUyw00MBJxEP/8uXL87Yy/CCURsUrLJIEtS7cpgspUfXW2JtWcohAZnlT/IZiwHa7XYhOZC+pqaUhsszqen9yNX/iOyG/771NsxzBMhlhWXjnCsUlQVEHCbHQZ542QhmIWumm0G1eq9ifd5fzlNlr3iFKv6g4bTO1r0fwHleFxcBg4FGiRVKs12KhNyJBRpvLY+qyY6f9sP/SkSNdOUAvF02WyUoKNqVbJx6Puds2/R6OwqPRgMrpmNrZQ7QH7szhNtyBbd6d12QCzc2T+N0w8bnMMBGbzDhG3KF0zqBqngv2mgqvb3qDtJn685KCVu6byMtmCs7fKcKlXSh6be7Q9EkC0S8YfXXbcW+UeZyXNLfjxIdbG+zTiuP8E4jMBIdKV041FPrfQoa6PD8cHQ6PB2dwdG80hCAYtJF4Feu0uW7eDwJkDhHj6gqq6LZDGgoMR4NjJs4ueXSz4H/2BPZik95e7FsPd8xNcYmybzL7Gp+cnNyPTx6dxg9OTw8Hk2fT4dNxHPvxKJ7F87Po+gbmFKZ54bpLy0EpfJdECk2pJIEEXapElfW+ut/lUQS0VzU/u2TZIF4ePhi4vfWrpb0qC4PLBb/KPBHORDXvZ3NhG3m7CQ1SO/H+Xq5vSzW0j7akoLd7SVrKy/6zAR9xhd837hH2Z7GKJcQmPnn8wH6P7ts5PIkf0lAenZ7Oj/3wnLaFA1G7nGsyYVF9azOs0CzOfxi0sHH60Pa12GYmPYMgjgP6w+Wwi6I9WBm87OI2PRewl6q7Cy1cTG/+XJn1BarvNwSfzCQ='
 
 def loggedin?(host)
 	fname = ENV['HOME']+'/.docker/config.json'
@@ -20,110 +27,177 @@ def loggedin?(host)
 	jso['auths'].include?(host) ? jso['auths'][host]['auth'] : nil
 end
 
-if ARGV.size<1
-	STDERR.puts 'pulldockerimage.rb host/repository:tag > archive.tar'
-	STDERR.puts 'generate a docker image directly (without deploying to the host machine)'
-	exit
-end
-
-repository = ARGV[0].split(':')[0]
-host = repository.split('/')[0]
-repository = repository[host.size+1..-1]
-tag = ARGV[0].split(':')[1]
-
-https = Net::HTTP.new(host,443)
-https.use_ssl = true
-https.verify_mode = OpenSSL::SSL::VERIFY_PEER
-https.start{
-	auth = {}
-        basic = loggedin?(host)
-	if basic
-		account = Base64.decode64(basic).split(':')[0]
-		resp = https.get(
-			'/auth?account=%s&scope=repository:%s:pull&service=%s'%[account,repository,host],
-			'Authorization' => 'Basic '+basic
-        	)
-		token = JSON.parse(resp.body)['token']
-		auth = {'Authorization' => 'Bearer '+token}
+def ensureResponse(resp,auth)
+	if ![301,302,307,308].include?(resp.code.to_i)
+		yield resp
+		return
 	end
-	resp = https.get(
-		'/v2/%s/manifests/%s'%[repository,tag],
-		auth.merge({
-			'Accept' => 'application/vnd.docker.distribution.manifest.v2+json'
-		})
-	)
-	manifestv2 = JSON.parse(resp.body)
-	Archive::Tar::Minitar::Output.open(STDOUT){|output|
-		tar = output.tar
-		resp = https.get(
-			'/v2/%s/blobs/%s'%[repository,manifestv2['config']['digest']],
-			auth
-		)
-		#File.write(manifestv2['config']['digest'].split(':')[1]+'.json',resp.body)
-		tar.add_file_simple(manifestv2['config']['digest'].split(':')[1]+'.json',{:data=>resp.body})
-		manifestjson = {
-			'Config' => manifestv2['config']['digest'].split(':')[1]+'.json',
-			'Repotags' => [
-				'%s/%s:%s'%[host,repository,tag]
-			],
-			'Layers' => []
-		}
-        	layerId = ''
-		manifestv2['layers'].each{|layer|
-			layerDigest = layer['digest']
-			parentId = layerId
-			layerId = Digest::SHA256.hexdigest(parentId+"\n"+layerDigest+"\n")
-			manifestjson['Layers'] << layerId+'/layer.tar'
-			STDERR.puts '%s (%s)'%[layerId,layerDigest]
-			#FileUtils.mkdir_p(layerId)
-			#File.write(layerId+'/VERSION','1.0')
-			tar.add_file_simple(layerId+'/VERSION',{:data=>'1.0'})
-			#File.write(layerId+'/json',
-			tar.add_file_simple(layerId+'/json',{:data=>
-				JSON.generate({
-					'id' => layerId,
-					'parentId' => parentId,
-					'created' => '0001-01-01T00:00:00Z',
-					'container_config' => {
-						'Hostname' => '',
-						'Domainname' => '',
-						'User' => '',
-						'AttachStdin' => false,
-						'AttachStdout' => false,
-						'AttachStderr' => false,
-						'Tty' => false,
-						'OpenStdin' => false,
-						'StdinOnce' => false,
-						'Env' => nil,
-						'Cmd' => nil,
-						'Image' => '',
-						'Volumes' => nil,
-						'WorkingDir' => '',
-						'Entrypoint' => nil,
-						'OnBuild' => nil,
-						'Labels' => nil,
-					}
-				})
-			})
-			
-			https.request_get(
-				'/v2/%s/blobs/%s'%[repository,layerDigest],
-				auth
-			){|resp|
-				#File.open(layerId+'/layer.tar','w'){|io|
-				tar.add_file_simple(layerId+'/layer.tar',{:size=>resp['content-length'].to_i}){|io|
-					resp.read_body{|body|io.write body}
-				}
+	resp.read_body
+	loop{
+		location = resp['location'].chomp
+		locationurl = URI.parse(location)
+		https = Net::HTTP.new(locationurl.host,443)
+		https.use_ssl = true
+		https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+		https.start{
+			https.request_get(locationurl.path+'?'+locationurl.query,auth){|resp|
+				if ![301,302,307,308].include?(resp.code.to_i)
+					yield resp
+					return
+				end
+				resp.read_body
 			}
 		}
-
-		#File.write('manifest.json',JSON.generate([manifestjson]))
-		tar.add_file_simple('manifest.json',{:data=>JSON.generate([manifestjson])})
-        	#File.write('repositories',
-		tar.add_file_simple('repositories',{:data=>
-			JSON.generate({
-				host+'/'+repository => {tag => ''}
-			})
-		})
 	}
+end
+
+def pullDockerImage(arg,fout)
+	repository = arg.split(':')[0]
+	host = repository.split('/')[0]
+	repository = repository[host.size+1..-1]
+	tag = arg.split(':')[1]
+
+	https = Net::HTTP.new(host,443)
+	https.use_ssl = true
+	https.verify_mode = OpenSSL::SSL::VERIFY_PEER
+	https.start{
+		auth = {}
+		resp = https.get(
+			'/v2/%s/manifests/%s'%[repository,tag],
+			auth.merge({
+				'Accept' => 'application/vnd.docker.distribution.manifest.v2+json'
+			})
+		)
+		if resp.code.to_i == 401
+			parser = Mechanize::HTTP::WWWAuthenticateParser.new.parse(resp['www-authenticate'])[0]
+			uri = URI.parse(parser.params['realm'])
+			basic = loggedin?(uri.host)
+			if basic
+				authhttps = Net::HTTP.new(uri.host,443)
+				authhttps.use_ssl = true
+				authhttps.verify_mode = OpenSSL::SSL::VERIFY_PEER
+				authhttps.start{
+					account = Base64.decode64(basic).split(':')[0]
+					resp = authhttps.get(
+						'%s?account=%s&scope=repository:%s:pull&service=%s'%[uri.path,account,repository,parser.params['service']],
+						'Authorization' => 'Basic '+basic
+					)
+					token = JSON.parse(resp.body)['token']
+					auth = {'Authorization' => 'Bearer '+token}
+				}
+			else
+				raise '`docker login %s` is required.'%host
+			end
+			resp = https.get(
+				'/v2/%s/manifests/%s'%[repository,tag],
+				auth.merge({
+					'Accept' => 'application/vnd.docker.distribution.manifest.v2+json'
+				})
+			)
+		end
+
+		manifestv2 = JSON.parse(resp.body)
+		Archive::Tar::Minitar::Output.open(fout){|output|
+			tar = output.tar
+			https.request_get(
+				'/v2/%s/blobs/%s'%[repository,manifestv2['config']['digest']],
+				auth
+			){|_resp|
+				ensureResponse(_resp,auth){|resp|
+					#File.write(manifestv2['config']['digest'].split(':')[1]+'.json',resp.read_body)
+					tar.add_file_simple(manifestv2['config']['digest'].split(':')[1]+'.json',{:data=>resp.read_body})
+				}
+			}
+			manifestjson = {
+				'Config' => manifestv2['config']['digest'].split(':')[1]+'.json',
+				'RepoTags' => [
+					'%s/%s:%s'%[host,repository,tag]
+				],
+				'Layers' => []
+			}
+			layerId = ''
+			manifestv2['layers'].each{|layer|
+				layerDigest = layer['digest']
+				parentId = layerId
+				layerId = Digest::SHA256.hexdigest(parentId+"\n"+layerDigest+"\n")
+				manifestjson['Layers'] << layerId+'/layer.tar'
+				STDERR.puts '%s (%s)'%[layerId,layerDigest]
+				#FileUtils.mkdir_p(layerId)
+				#File.write(layerId+'/VERSION','1.0')
+				tar.add_file_simple(layerId+'/VERSION',{:data=>'1.0'})
+				#File.write(layerId+'/json',
+				tar.add_file_simple(layerId+'/json',{:data=>
+					JSON.generate({
+						'id' => layerId,
+						'parentId' => parentId,
+						'created' => '0001-01-01T00:00:00Z',
+						'container_config' => {
+							'Hostname' => '',
+							'Domainname' => '',
+							'User' => '',
+							'AttachStdin' => false,
+							'AttachStdout' => false,
+							'AttachStderr' => false,
+							'Tty' => false,
+							'OpenStdin' => false,
+							'StdinOnce' => false,
+							'Env' => nil,
+							'Cmd' => nil,
+							'Image' => '',
+							'Volumes' => nil,
+							'WorkingDir' => '',
+							'Entrypoint' => nil,
+							'OnBuild' => nil,
+							'Labels' => nil,
+						}
+					})
+				})
+
+				https.request_get(
+					'/v2/%s/blobs/%s'%[repository,layerDigest],
+					auth
+				){|_resp|
+					ensureResponse(_resp,auth){|resp|
+						#File.open(layerId+'/layer.tar','w'){|io|
+						tar.add_file_simple(layerId+'/layer.tar',{:size=>resp['content-length'].to_i}){|io|
+							resp.read_body{|body|io.write body}
+						}
+					}
+				}
+			}
+
+			#File.write('manifest.json',JSON.generate([manifestjson]))
+			tar.add_file_simple('manifest.json',{:data=>JSON.generate([manifestjson])})
+			#File.write('repositories',
+			tar.add_file_simple('repositories',{:data=>
+				JSON.generate({
+					host+'/'+repository => {tag => ''}
+				})
+			})
+		}
+	}
+	return 0
+end
+
+if __FILE__ == $0
+	if ARGV.size<1
+		STDERR.puts <<EOM
+pulldockerimage.rb host/repository:tag > archive.tar
+eg: index.docker.io/library/ubuntu:devel > ubuntu.tar
+
+generate a docker image directly (without deploying to the client machine).
+
+`docker login` is required prior. .docker/config.json should look like this.
+
+{
+        "auths": {
+                "index.docker.io": {
+                        "auth": base64(username:password)
+                }
+        }
 }
+EOM
+		exit 1
+	end
+	exit pullDockerImage ARGV[0], STDOUT
+end
